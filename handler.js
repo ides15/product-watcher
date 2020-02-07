7f6b01f1f3d640d82bb855f306398bad230e84fa
John Ide <john.ide@missionessential.com>
2020-02-07 10:07:45 -0500

    Trying to get secrets into container

--- handler.js
+++ handler.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const email = require("./email");
 const s3 = require("./s3");
 const scraper = require("./scraper");
