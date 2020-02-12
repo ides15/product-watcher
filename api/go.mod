a619e0927c673cea4388f7c5dff7982cb6d4c0b7
John Ide <john.ide@missionessential.com>
2020-02-11 23:00:35 -0500

    Connecting to s3

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,8 @@
 module github.com/ides15/product-watcher/api
 
 go 1.13
+
+require (
+	github.com/aws/aws-sdk-go v1.28.13 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- api/main.go
+++ api/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	sess, err := session.NewSession(&aws.Config{
+		Credentials: credentials.NewEnvCredentials(),
+		Region:      aws.String("us-east-1")},
+	)
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	result, err := svc.ListBuckets(nil)
+	if err != nil {
+		fmt.Errorf("Unable to list buckets, %v", err)
+	}
+
+	fmt.Println("Buckets:")
+
+	for _, b := range result.Buckets {
+		fmt.Printf("* %s created on %s\n",
+			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+	}
 }
